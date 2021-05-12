e2ff7daa50762cd14fd1f34f1d6f20e4d96a2f72
siddhant <siddhantkhare2694@gmail.com>
2021-05-12 16:48:45 +0530

    feat:Bash Script of fetch data from URL using JS ft. ZX

--- fetchDataFromURL.mjs
+++ fetchDataFromURL.mjs
@@ -1,9 +1,8 @@
 #!/usr/bin/env zx
 
-
 // it returns the weather report of your location
 
-let resp = await fetch('http://wttr.in')
+let resp = await fetch("http://wttr.in");
 if (resp.ok) {
   console.log(await resp.text());
 }
